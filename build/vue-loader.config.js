5dd57c44a4ddbbdaa4f6a618f05bd0569d243ce1
LAPTOP-PKNEGM2Q\superman <caixiaolin@huxiu.com>
2018-10-25 16:45:25 +0800

    删除给自定义模块配置自定义loader,不常用，本项目有暂不考虑

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,14 +1,7 @@
-const docsLoader = require.resolve('./docs-loader')
-
 module.exports = (isDev) => {
   return {
     preserveWhitespace: true,
     extractCSS: !isDev, //单独打包css（生产环境可设置单独打包dev）
     cssModules: {},
     // hotReload: false  //热重载功能(根据环境变量生成)
-    // loaders: {
-    //   'docs': docsLoader,   //给自定义的模块配置自定义的loader
-    // },
-    // preLoader: {},
-    // postLoader: {}    loaders,preLoader,postLoader不常用
   }}
